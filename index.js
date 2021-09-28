acc5c869734539339b971efc505361ec72f338b6
Martin Haiss <haissmartin@gmail.com>
2021-09-28 09:19:58 +0200

    sixth

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ const app = express()
 
 const port = 3000
 
+app.use(express.json())
+
 app.get('/api/cards', (request, response) => {
   response.set('Content-type', 'text/html; charset=utf-8')
   response.send('<h1>Hello, World! Wie läufts?</h1>')
 })
 
 app.post('/api/cards', (request, response) => {
-  response.send('This was a post request')
+  response.send('This was a POST request')
 })
 
 app.put('/api/cards', (request, response) => {
-  response.send('This was a put request')
+  response.send('This was a PUT request')
 })
 
 app.patch('/api/cards', (request, response) => {
-  response.send('This was a patch request')
+  response.send('This was a PATCH request')
 })
 
 app.delete('/api/cards', (request, response) => {
-  response.send('This was a delete request')
+  response.send('This was a DELETE request')
 })
 
 app.listen(port, () => {
