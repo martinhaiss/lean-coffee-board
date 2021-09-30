3f7ba39ce98612c5af9aafc47956fd94881ed616
Martin Haiss <haissmartin@gmail.com>
2021-09-30 12:55:05 +0200

    add error-handler

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const connectDatabase = require('./setupMongo')
+const errorHandler = require('./errorHandler')
+
 const app = express()
 
 const port = 3000
@@ -9,6 +11,8 @@ connectDatabase('mongodb://localhost:27017/lean-coffee-board')
 app.use(express.json())
 
 app.use('/api/cards', require('./routes/cards'))
+
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
